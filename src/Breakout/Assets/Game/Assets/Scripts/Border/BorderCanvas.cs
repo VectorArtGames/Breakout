e3f41a545aaf89406db0ffc3c3655934fe29d8fa
VectorArtGames <70201838+VectorArtGames@users.noreply.github.com>
2020-10-05 22:04:44 +0200

    initial release

---
+++ src/Breakout/Assets/Game/Assets/Scripts/Border/BorderCanvas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderCanvas : MonoBehaviour
+{
+	public Camera cam;
+	public RectTransform ContainerCanvas;
+
+	public float Width;
+	public float Height;
+
+	#region Singleton
+
+	public static BorderCanvas Instance { get; set; }
+
+	private void Awake()
+	{
+		Instance = this;
+		cam = Camera.main;
+	}
+
+	#endregion
+
+	private void Start()
+	{
+		UpdateBorder();
+	}
+
+	public void UpdateBorder()
+	{
+		if (!(ContainerCanvas is RectTransform t && t.rect is Rect rect)) return;
+		var aspect = cam.orthographicSize / cam.aspect;
+		Width = (rect.width - 50) / aspect;
+		Height = (rect.height - 50) / aspect;
+	}
+
+	public void OnRectTransformDimensionsChange()
+	{
+		UpdateBorder();
+	}
+
+}
---
+++ src/Breakout/Assets/Game/Assets/Scripts/Paddle/PaddleMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMove : MonoBehaviour
+{
+	public Camera cam;
+
+	public BorderCanvas border;
+    // Start is called before the first frame update
+    void Start()
+    {
+        border = BorderCanvas.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    if (cam == null || border.Width <= 0 || border.Height <= 0) return;
+
+        if (!(transform is Transform t && t.localPosition is Vector3 pos)) return;
+
+        var posX = Mathf.Clamp(Input.mousePosition.x - (border.Width / 2), -border.Width, border.Width);
+
+        Debug.Log(posX);
+
+        t.localPosition = new Vector3(posX, pos.y, pos.z);
+    }
+}
